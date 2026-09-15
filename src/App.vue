d0b13bd3ec31a5a3684c0aaffa8f2d911e4ff8ef
Qcivel <q.civel@yahoo.fr>
2026-09-15 12:14:02 +0200

    add battle tag v.1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import PlayerComponent from "./components/PlayerComponent.vue";
+import BattleLog from "./components/BattleLog.vue";
 </script>
 
+
 <template>
+  <BattleLog/>
   <PlayerComponent/>
 </template>
