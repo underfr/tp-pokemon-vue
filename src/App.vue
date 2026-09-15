7908693e737d655390395e174b4245401caa77f8
underfr <loicklaurent28@gmail.com>
2026-09-15 10:29:58 +0200

    Add: DaisyUI + PlayerComponent init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import PlayerComponent from "./components/PlayerComponent.vue";
 </script>
 
 <template>
-  <HelloWorld />
+  <PlayerComponent/>
 </template>
